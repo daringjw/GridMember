df08769a2e92f39502bcd5fd840b30cfaaecd98d
guan <909569736@qq.com>
2017-12-27 11:10:58 +0800

    新建工程

---
+++ GridMember/app/src/main/java/com/jkkc/gridmember/common/Config.java
@@ -0,0 +1,8 @@
+package com.jkkc.gridmember.common;
+
+/**
+ * Created by Guan on 2017/12/27.
+ */
+
+public class Config {
+}
---
+++ GridMember/app/src/main/java/com/jkkc/gridmember/ui/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.jkkc.gridmember.ui;
+
+/**
+ * Created by Guan on 2017/12/27.
+ */
+
+public class LoginActivity {
+}
