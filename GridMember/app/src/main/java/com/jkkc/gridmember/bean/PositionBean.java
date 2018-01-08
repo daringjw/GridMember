0b0eb6fd78f9c17dadf5c2e1204f133fe510d481
guan <909569736@qq.com>
2018-01-08 11:09:37 +0800

    封装positionManager

---
+++ GridMember/app/src/main/java/com/jkkc/gridmember/bean/PositionBean.java
@@ -0,0 +1,12 @@
+package com.jkkc.gridmember.bean;
+
+/**
+ * Created by Guan on 2018/1/8.
+ */
+
+public class PositionBean {
+
+    public double latitude; //纬度
+    public double longtitude; //经度
+
+}
